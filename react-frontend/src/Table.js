b9f9998c2192b96683c12d28d442b2cb1b781144
hmaziz <102627606+hmaziz@users.noreply.github.com>
2023-04-10 09:20:54 -0700

    Part 2-1 Window is blank

--- react-frontend/src/Table.js
+++ react-frontend/src/Table.js
@@ -20,19 +20,30 @@ function TableBody(props) {
    }
   );
   return (
-      <tbody>
-        {rows}
-       </tbody>
+    <tr key={index}>
+    <td>{row.name}</td>
+    <td>{row.job}</td>
+    <td>
+      <button onClick={() => props.removeCharacter(index)}>Delete</button>
+    </td>
+  </tr>
    );
 }
-
-
-function Table(props) {
-    return (
-      <table>
-        <TableHeader />
-        <TableBody characterData={props.characterData} />
-      </table>
-    );
+function Table (props) {
+  return (
+    <table>
+      <TableHeader />
+      <TableBody characterData={props.characterData} removeCharacter={props.removeCharacter} />
+    </table>
+  );
 }
+
+// function Table(props) {
+//     return (
+//       <table>
+//         <TableHeader />
+//         <TableBody characterData={props.characterData} />
+//       </table>
+//     );
+// }
 export default Table;
